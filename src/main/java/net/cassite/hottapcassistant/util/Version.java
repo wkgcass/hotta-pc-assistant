46a5dc003f36c68d318e25679be504b138715dc7
wkgcass <wkgcass@hotmail.com>
2022-12-05 14:51:04 +0800

    update version to 0.0.2-dev

--- src/main/java/net/cassite/hottapcassistant/util/Version.java
+++ src/main/java/net/cassite/hottapcassistant/util/Version.java
@@ -1,7 +1,7 @@
 package net.cassite.hottapcassistant.util;
 
 public class Version {
-    public static final String version = "0.0.1-dev";
+    public static final String version = "0.0.2-dev";
 
     private Version() {
     }
