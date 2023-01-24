2d587d74271e3bde12cdf4998fe38650285b7b71
wkgcass <wkgcass@hotmail.com>
2023-01-24 19:35:24 +0800

    0.0.3 release

--- src/main/java/net/cassite/hottapcassistant/util/Version.java
+++ src/main/java/net/cassite/hottapcassistant/util/Version.java
@@ -1,7 +1,7 @@
 package net.cassite.hottapcassistant.util;
 
 public class Version {
-    public static final String version = "0.0.3-dev";
+    public static final String version = "0.0.3";
 
     private Version() {
     }
