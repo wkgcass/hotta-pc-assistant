f3452b5a5133a9f1739413249e5b395a5aef0d4a
wkgcass <wkgcass@hotmail.com>
2023-02-05 16:14:37 +0800

    start 0.0.3-addition

--- src/main/java/net/cassite/hottapcassistant/util/Version.java
+++ src/main/java/net/cassite/hottapcassistant/util/Version.java
@@ -1,7 +1,7 @@
 package net.cassite.hottapcassistant.util;
 
 public class Version {
-    public static final String version = "0.0.3";
+    public static final String version = "0.0.3-addition";
 
     private Version() {
     }
