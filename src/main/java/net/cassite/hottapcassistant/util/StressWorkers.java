d3b34877e40185e328f1b19f4fa76d2e47c35a21
wkgcass <wkgcass@hotmail.com>
2024-01-18 15:02:17 +0800

    [stress] call windows api to set process priority

--- src/main/java/net/cassite/hottapcassistant/util/StressWorkers.java
+++ src/main/java/net/cassite/hottapcassistant/util/StressWorkers.java
@@ -1,5 +1,8 @@
 package net.cassite.hottapcassistant.util;
 
+import com.sun.jna.Native;
+import com.sun.jna.win32.W32APIOptions;
+
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class StressWorkers {
@@ -14,11 +17,31 @@ public class StressWorkers {
 
     private final AtomicBoolean running = new AtomicBoolean(false);
     private int cores = 0;
+    private final AtomicBoolean prioritySet = new AtomicBoolean(false);
+
+    private void setPriority() {
+        if (!prioritySet.compareAndSet(false, true)) {
+            return;
+        }
+        var p = Kernel32.INSTANCE.GetCurrentProcess();
+        Kernel32.INSTANCE.SetPriorityClass(p, Kernel32.REALTIME_PRIORITY_CLASS);
+    }
+
+    private interface Kernel32 extends com.sun.jna.platform.win32.Kernel32 {
+        Kernel32 INSTANCE = Native.load(Kernel32.class, W32APIOptions.UNICODE_OPTIONS);
+        DWORD REALTIME_PRIORITY_CLASS = new DWORD(0x00000100);
+
+        HANDLE GetCurrentProcess();
+
+        @SuppressWarnings("UnusedReturnValue")
+        boolean SetPriorityClass(HANDLE hProcess, DWORD dwPriorityClass);
+    }
 
     public void begin() {
         if (!running.compareAndSet(false, true)) {
             return;
         }
+        setPriority();
         if (cores == 0) {
             cores = Runtime.getRuntime().availableProcessors();
         }
