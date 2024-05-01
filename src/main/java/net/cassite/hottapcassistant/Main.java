8ecd90022fd66ed7b64eaf71ddf948e7bee2c634
wkgcass <wkgcass@hotmail.com>
2024-05-02 01:53:04 +0800

    [icon] change program icon

--- src/main/java/net/cassite/hottapcassistant/Main.java
+++ src/main/java/net/cassite/hottapcassistant/Main.java
@@ -108,7 +108,7 @@ public class Main extends Application {
             title = title + " " + I18n.get().titleMainScreenDevVersion();
         }
         stage.setTitle(title);
-        stage.getStage().getIcons().add(ImageManager.get().load("images/icon/moliniya.png"));
+        stage.getStage().getIcons().add(ImageManager.get().load("images/icon/Avatar_150.png"));
         stage.getStage().setWidth(1100);
         stage.getStage().setHeight(800);
         stage.getStage().centerOnScreen();
