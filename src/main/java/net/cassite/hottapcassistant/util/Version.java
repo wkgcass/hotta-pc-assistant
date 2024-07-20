3b39433205a695a1b60f166dfa64c8e0c5ce65fb
K.G. Wang <wkgcass@hotmail.com>
2024-07-21 04:38:22 +0800

    new dev release

--- src/main/java/net/cassite/hottapcassistant/util/Version.java
+++ src/main/java/net/cassite/hottapcassistant/util/Version.java
@@ -1,8 +1,8 @@
 package net.cassite.hottapcassistant.util;
 
 public class Version {
-    public static final String version = "1.1.0-BETA-4";
-    public static final String devVersion = "2024-05-27_01";
+    public static final String version = "1.1.0-BETA-5";
+    public static final String devVersion = "2024-07-21_01";
 
     private Version() {
     }
